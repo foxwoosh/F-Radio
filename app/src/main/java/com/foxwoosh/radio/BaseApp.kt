37f4d27294c324cd8e5ef74ce7d73bec594c15f8
Den <enter.your.thoughts@gmail.com>
2022-06-13 18:21:19 +0300

    fix [crashlytics] - enabled for release only

--- app/src/main/java/com/foxwoosh/radio/BaseApp.kt
+++ app/src/main/java/com/foxwoosh/radio/BaseApp.kt
@@ -3,6 +3,7 @@ package com.foxwoosh.radio
 import android.app.Application
 import android.os.Build
 import com.foxwoosh.radio.notifications.NotificationPublisher
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -14,5 +15,7 @@ class BaseApp : Application() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationPublisher.createChannels(this)
         }
+
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(!BuildConfig.DEBUG)
     }
 }
